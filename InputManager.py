0411db85ef23041934f8a53b66b66dfa640b5d90
Esty Katzeff <esty.katzeff@gmail.com>
2022-03-21 11:12:37 +0200

    managing organism input

--- InputManager.py
+++ InputManager.py
@@ -4,6 +4,7 @@ import os.path
 import SharedConsts as sc
 import pickle
 from datetime import datetime
+from utils import logger
 
 SERVER_PATH = r"ftp.ncbi.nlm.nih.gov"
 SERVER_BACTERIA_LOCATION = r"/genomes/refseq/bacteria/"
@@ -25,7 +26,7 @@ class InputManager:
 
                 ftp = FTP(SERVER_PATH)
                 ftp.login()
-                print('logged in')
+                logger.info(f'logged in {ftp}')
 
                 ftp.cwd(SERVER_BACTERIA_LOCATION)
                 print(f'changed dir to {SERVER_BACTERIA_LOCATION}')
@@ -42,25 +43,32 @@ class InputManager:
 
     def __need_to_update_bacteria_list(self):
         bacteria_list_path = sc.PATH2BACTERIAS_LIST
-        if not os.path.isfile(sc.PATH2BACTERIAS_LIST): #file doesn't exist yet
+        if not os.path.isfile(bacteria_list_path): #file doesn't exist yet
             return True
         last_modified_date = datetime.fromtimestamp(os.path.getmtime(bacteria_list_path)) #make sure this is created date if the file wasn't ever modified
         now = datetime.now()
         delta = now - last_modified_date
         if delta.days > 0:  # hasn't been updated today
+            os.remove(bacteria_list_path) # delete file
+            logger.info(f'file bacteria_list_path was deleted = {os.path.isfile(bacteria_list_path)}')
             return True
         return False
 
     def validate_bacteria_input(self, bacteria_input):
+        logger.info(f'validating organism input')
         bacteria_list_path = sc.PATH2BACTERIAS_LIST
         if self.__need_to_update_bacteria_list():
+            logger.info(f'updating bacteria list in {bacteria_list_path}')
             bacteria_list = self.__get_bacteria_list()
             with open(bacteria_list_path, 'wb') as f:
                 pickle.dump(bacteria_list, f)
+            logger.info(f'finished updating bacteria list')
         else:  # no need to update list
-            with open(bacteria_list_path) as f:
+            logger.info(f'no need to update bacteria list')
+            with open(bacteria_list_path, 'rb') as f:
                 bacteria_list = pickle.load(f)
         bacterias_of_interest = self.__get_bacteria_of_interest_paths_list(bacteria_input, bacteria_list)
+        logger.info(f'bacterias_of_interest = {bacterias_of_interest}')
         return len(bacterias_of_interest) > 0
 
     def __get_bacteria_of_interest_paths_list(self, bacteria_input, bacteria_list):
