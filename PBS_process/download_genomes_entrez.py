d696cacc1ea0f0e0a799f1137d2c17297b531aa4
Esty Katzeff <esty.katzeff@gmail.com>
2022-06-30 15:01:50 +0300

    making order in download assemblies code

--- PBS_process/download_genomes_entrez.py
+++ PBS_process/download_genomes_entrez.py
@@ -9,11 +9,11 @@ import traceback
 TODO:
 - test more to see that it downloads like assembly ncbi
 - downloading only latest_refseq
-- test Rickettsiales
+- test Rickettsiales - works on flask, but not on apache
 """
 
 SERVER_PATH = r"ftp.ncbi.nlm.nih.gov"
-NUMBER_OF_TRIES = 5 # change to bigger number once stable
+NUMBER_OF_TRIES = 5  # change to bigger number once stable
 count_failed_download = [0]
 
 def get_assembly_summary(id):
@@ -40,7 +40,7 @@ def download_files(url_genbank, save_folder, is_fasta=False):
     label = os.path.basename(url_genbank)
     if is_fasta:
         file_ending = 'genomic.fna.gz'
-    else: # stats ending
+    else:  # stats
         file_ending = 'assembly_stats.txt'
     file2download = f'{url_genbank}/{label}_{file_ending}'.split(SERVER_PATH)[-1]  # get the download link
     local_filename = os.path.join(save_folder, os.path.basename(os.path.normpath(file2download)))
@@ -50,8 +50,8 @@ def download_files(url_genbank, save_folder, is_fasta=False):
         return local_filename
 
     file = open(local_filename, 'wb')
-    try:
-        ftp = FTP(SERVER_PATH)  # not sure how long this timeout should be
+    try:  # creating a new to ftp connection per file (to prevent a timeout)
+        ftp = FTP(SERVER_PATH)
         ftp.login()
         print('logged in')
         ftp.retrbinary('RETR ' + file2download, file.write)
@@ -63,7 +63,7 @@ def download_files(url_genbank, save_folder, is_fasta=False):
         except Exception as e:
             print(f'failed to close ftp connection. Error: {e}')
             print(traceback.format_exc())
-    except Exception as e:
+    except Exception as e:  # catch failed to connect/ failed to
         count_failed_download[0] += 1
         print(f'error downloading file2download: {file2download}, url_genbank: {url_genbank}, Error: {e}')
         print(e)
@@ -88,10 +88,6 @@ def get_assemblies(save_folder, term):
     print(f'found {len(ids)} ids')
     refseq_assemblies_count = 0
 
-    # ftp = FTP(SERVER_PATH) # not sure how long this timeout should be
-    # ftp.login()
-    # print('logged in')
-
     results_folder, fasta_folder, stats_folder = init_dirs(save_folder)
 
     for id in ids:
@@ -108,7 +104,7 @@ def get_assemblies(save_folder, term):
         # download stats
         stats_file_path = download_files(url_genbank, stats_folder)
 
-        # checking if refseq
+        # checking if assembly is refseq
         if os.path.isfile(stats_file_path):
             with open(stats_file_path) as stats_file:
                 for line in stats_file:
@@ -117,22 +113,13 @@ def get_assemblies(save_folder, term):
                         refseq_assemblies_count += 1
 
         if is_refseq: # only download assemblies which are refseq (Future: add this as option?)
-            # download assemblies
-            fasta_file_path = download_files(url_genbank, fasta_folder, is_fasta=True)
-
+            fasta_file_path = download_files(url_genbank, fasta_folder, is_fasta=True)  # download assemblies
         else:
             print(f'{url_genbank} is not a refseq. skipping.')
 
-    # try: # sometimes quits on its own
-    #     ftp.quit()  # This is the “polite” way to close a connection
-    #     print("Successfully quited ftp connection")
-    # except Exception as e:
-    #     print(f'failed to close ftp connection. Error: {e}')
-    #     print(traceback.format_exc())
-
     end = datetime.now()
     print(f'average download time per file: {(end - start_all) / len(ids)}')
-    print(f'time: {end - start_all} seconds')
+    print(f'time: {end - start_all}')
     print(f'refseq_assemblies_count: {refseq_assemblies_count}')
 
     # zipping the fasta folder
@@ -142,18 +129,6 @@ def get_assemblies(save_folder, term):
 
 def run(save_folder, term, filter_by_level=False, filter_by_date=False):
     get_assemblies(save_folder, term)
-    # for i in range(NUMBER_OF_TRIES):
-    #     try:
-    #         print(f'===============Try #{i}=================')
-    #         get_assemblies(save_folder, term)
-    #     except Exception as e:
-    #         print(f'finished try number: {i} out of {NUMBER_OF_TRIES-1}')
-    #         print(e)
-    #         print(traceback.format_exc())
-    #     else:
-    #         print("Exiting")
-    #         break
-
 
 
 def main():
